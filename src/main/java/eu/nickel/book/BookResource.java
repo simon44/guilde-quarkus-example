141687136c1e299e190b7d895bc15a8ee55d22b1
Simon BERNARD <simon44360@gmail.com>
2021-05-06 17:49:01 +0200

    Complete CRUD endpoints, and add swagger-ui

--- src/main/java/eu/nickel/book/BookResource.java
+++ src/main/java/eu/nickel/book/BookResource.java
@@ -1,12 +1,23 @@
 package eu.nickel.book;
 
+import eu.nickel.book.payload.BookPayload;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import javax.ws.rs.core.Response;
 
 @Path("/books")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
     private final BookService bookService;
@@ -16,8 +27,34 @@ public class BookResource {
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Book> book() {
-        return bookService.getAll();
+    public Response getAll() {
+        return Response.ok().entity(bookService.getAll()).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getById(@PathParam("id") int id) {
+        return Response.ok().entity(bookService.getById(id)).build();
+    }
+
+    @POST
+    @Transactional
+    public Response create(@Valid BookPayload bookPayload) {
+        return Response.ok(bookService.create(bookPayload)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response update(@PathParam("id") int id, @Valid BookPayload bookPayload) {
+        return Response.ok(bookService.update(id, bookPayload)).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") int id) {
+        bookService.delete(id);
+        return Response.noContent().build();
     }
 }
--- src/main/java/eu/nickel/book/BookService.java
+++ src/main/java/eu/nickel/book/BookService.java
@@ -1,12 +1,49 @@
 package eu.nickel.book;
 
+import eu.nickel.book.payload.BookPayload;
+import io.netty.handler.codec.http.HttpResponseStatus;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.WebApplicationException;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class BookService {
 
     public List<Book> getAll() {
         return Book.findAll().list();
+    }
+
+    public Book getById(int id) {
+        Book book = Book.findById(id);
+        return Optional.ofNullable(book)
+                .orElseThrow(() -> new WebApplicationException(HttpResponseStatus.NOT_FOUND.code()));
+    }
+
+    public Book create(BookPayload bookPayload) {
+        Book book = new Book();
+        book.title = bookPayload.getTitle();
+        book.persist();
+        return book;
+    }
+
+    public Book update(int id, BookPayload bookPayload) {
+        Book book = Book.findById(id);
+        return Optional.ofNullable(book)
+                .map(b -> {
+                    b.title = bookPayload.getTitle();
+                    b.persist();
+                    return b;
+                }).orElseThrow(() -> new WebApplicationException(HttpResponseStatus.NOT_FOUND.code()));
+    }
+
+    public void delete(int id) {
+        Book book = Book.findById(id);
+        if (book == null) {
+            throw new WebApplicationException(HttpResponseStatus.NOT_FOUND.code());
+        } else {
+            book.delete();
+        }
     }
 }
